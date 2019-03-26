f5c8831fa47369cd1bea0c8911b31af2c561cb1d
xremix <tonidhoffmann@gmail.com>
2019-03-26 11:25:36 +0100

    Init Empty Project

---
+++ Weather Recognizer App/Weather Recognizer App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Recognizer App
+//
+//  Created by Toni Hoffmann on 26.03.19.
+//  Copyright © 2019 Toni Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Weather Recognizer App/Weather Recognizer AppTests/Weather_Recognizer_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_Recognizer_AppTests.swift
+//  Weather Recognizer AppTests
+//
+//  Created by Toni Hoffmann on 26.03.19.
+//  Copyright © 2019 Toni Hoffmann. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_Recognizer_App
+
+class Weather_Recognizer_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
